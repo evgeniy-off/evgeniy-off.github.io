eb26887163aec9cf46ff4d31b846e1879f7a5623
Evgeniy <42904603+evgeniy-off@users.noreply.github.com>
2018-10-13 20:13:07 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-$('#header-img img').on('click', function(){
-    $(this).alert('как ты думаешь, слоник и правда зеленый?');
-});
+$('#header-btn').on('click', function(e){
+    e.preventDefault();
+    $(this).alert('ghfb[');
 
+    
+});
 
 
 });
