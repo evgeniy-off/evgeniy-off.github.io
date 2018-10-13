5a301596b9d49d6247e2b1d90fb6ffc4558e1e08
Evgeniy <42904603+evgeniy-off@users.noreply.github.com>
2018-10-13 19:41:35 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $('#header-img img').on('click', function(){
-    $('#header-img img').alert('как ты думаешь, слоник и правда зеленый?');
+    $(this).alert('как ты думаешь, слоник и правда зеленый?');
 
 });
 
